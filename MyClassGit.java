94bd59d1b1bce3c4603f9a70c07c70447230f840
shkulepova <43042851+shkulepova@users.noreply.github.com>
2018-09-06 19:25:33 +0300

    Add files via upload

---
+++ MyClassGit.java
@@ -0,0 +1,20 @@
+package hw3_git;
+
+public class MyClassGit {
+
+	public static void main(String[] args) {
+		System.out.println("I created a first class");
+		System.out.println("Commit # 1.0");
+		
+		System.out.println("I added MyNewClass");
+		System.out.println("Commit # 2.0");
+		
+		System.out.println("I added constructor to class MyNewClass");
+		System.out.println("Commit # 3.0");
+		
+		System.out.println("I added method to class MyNewClass");
+		System.out.println("Commit # 4.0");
+
+	}
+
+}
